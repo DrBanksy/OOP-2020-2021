bf8e9f68fb4cbd50b7392e4c7fd0a1f852bf7bcc
cormac smith <cormacsmith@live.co.uk>
2021-03-24 18:23:54 +0000

    bla

---
+++ java/src/ie/tudublin/Health.java
@@ -0,0 +1,26 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Health {
+    float x, y;
+    float dX, dY;
+
+    public Health(YASC yasc, float x, float y, float dX, float dY) {
+        this.x = x;
+        this.y = y;
+        this.dX = dX;
+        this.dY = dY;
+    }
+
+    void render()
+    {
+        
+        
+    }
+
+    void respawn() {
+
+    }
+
+}
