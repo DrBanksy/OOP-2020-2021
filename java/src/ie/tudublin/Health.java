55013bba0fefa704ebe2503d655bf40897f95f7c
cormac smith <cormacsmith@live.co.uk>
2021-03-30 11:41:46 +0100

    bla

--- java/src/ie/tudublin/Health.java
+++ java/src/ie/tudublin/Health.java
@@ -5,12 +5,21 @@ import processing.core.PApplet;
 public class Health {
     float x, y;
     float dX, dY;
+    float w = 50;
+    float halfW = w / 2;
+    YASC yasc;
+    float rotation;
 
     public Health(YASC yasc, float x, float y, float dX, float dY) {
-        this.x = x;
-        this.y = y;
-        this.dX = dX;
-        this.dY = dY;
+        int dice = (int)yasc.random(4);
+        switch(dice)
+        {
+            case 0 :
+                x = -halfW;
+                y = yasc.random(halfW, yasc.height-halfW);
+                dX = yasc.random(1, 4);
+                dY = yasc.random(-1, 1);
+        }
     }
 
     void render()
